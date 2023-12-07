cd64e6935eaf21d2d0efc834b5718bb0a1e432df
neoscript <neoscripting@gmail.com>
2023-12-07 11:13:24 +0000

    Add day-01 part1 solution

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,9 +1,40 @@
 fn main(){
-    println!("part 1 running.");
+    let input = include_str!("./input1.txt");
+    let result = process(input);
+    println!("{}", result);
 }
 
 fn process(input: &str) -> String{
-    "result1".to_string()
+    let lines: Vec<&str> = input.split("\n").collect();
+    let mut values: Vec<i32> = vec![];
+
+    for mut line in lines{
+        line = line.trim();
+        let (mut d1, mut d2) = (-1,-1);
+        
+        for char in line.chars(){
+            if char.is_numeric(){
+                if d1 == -1{
+                    d1 = char.to_string().parse().unwrap();
+                }else{
+                    d2 = char.to_string().parse().unwrap();
+                }
+            }
+        }
+
+        if d2 == -1{
+            d2 = d1;
+        }
+
+        let line_value = (d1 * 10) + d2;
+        values.push(line_value)
+    }
+
+    println!("values -> {:?}", values);
+    let sum: i32 = values.iter().sum();
+    let result = format!("{}", sum);
+    
+    result
 }
 
 #[cfg(test)]
@@ -12,7 +43,10 @@ mod tests{
 
     #[test]
     fn test_process(){
-        let result = process("value");
-        assert_eq!(result, "result1".to_string());
+        let result = process("1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet");
+        assert_eq!(result, "142".to_string());
     }
 }
